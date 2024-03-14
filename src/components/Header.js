8764a878698c86f17ebae70f2c3ceb89c375b3aa
jadelcruz1 <jardelcruz6@gmail.com>
2024-03-14 16:27:30 -0300

    novo header com menu lateral

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,39 +1,128 @@
-// Header.js
-import React from 'react';
+import * as React from 'react';
 import { Link } from 'react-router-dom';
-import './Header.css'; 
+import { styled, alpha } from '@mui/material/styles';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import InputBase from '@mui/material/InputBase';
+import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
+import Drawer from '@mui/material/Drawer';
 
-const Header = () => {
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(1),
+    width: 'auto',
+  },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  width: '100%',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    [theme.breakpoints.up('sm')]: {
+      width: '12ch',
+      '&:focus': {
+        width: '20ch',
+      },
+    },
+  },
+}));
+
+export default function SearchAppBar() {
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
   return (
-    <header className="header">
-      <div className="container">
-        <div className="logo">
-          <Link to="/">UniSus</Link>
-        </div>
-        <nav className="nav">
-          <ul>
-            <li>
-              <Link to="/">Dashboard</Link>
-            </li>
-            <li>
-              <Link to="/cadastro">Cadastro Paciente</Link>
-            </li>
-            <li>
-              <Link to="/CadastroMedico">Cadastro Médicos</Link>
-            </li>
-            <li>
-              <Link to="/profile">Usuários Cadastrados</Link>
-            </li>   
-            <li>
-              <Link to="/medicosCadastrados">Médicos Cadastrados</Link>
-            </li>   
-
-          
-          </ul>
-        </nav>
-      </div>
-    </header>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="open drawer"
+            onClick={() => setIsMenuOpen(!isMenuOpen)}
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+          >
+            UniSus
+          </Typography>
+          <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Search…"
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </Search>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        anchor="left"
+        open={isMenuOpen}
+        onClose={() => setIsMenuOpen(false)}
+      >
+        <Box sx={{ width: 250 }}>
+        
+          <Link to="/" style={{ textDecoration: 'none', color: 'inherit' }}>
+            <Typography variant="h6" sx={{ p: 2 }}>
+           Home
+            </Typography>
+          </Link>
+          <Link to="/cadastro" style={{ textDecoration: 'none', color: 'inherit' }}>
+            <Typography variant="h6" sx={{ p: 2 }}>
+            Cadastro Paciente
+            </Typography>
+          </Link>
+          <Link to="/CadastroMedico" style={{ textDecoration: 'none', color: 'inherit' }}>
+            <Typography variant="h6" sx={{ p: 2 }}>
+              Cadastro Médicos
+            </Typography>
+          </Link>
+          <Link to="/profile" style={{ textDecoration: 'none', color: 'inherit' }}>
+            <Typography variant="h6" sx={{ p: 2 }}>
+            Usuários Cadastrados
+            </Typography>
+           <Link to="/medicosCadastrados" style={{ textDecoration: 'none', color: 'inherit' }}>
+            <Typography variant="h6" sx={{ p: 2 }}>
+            Médicos Cadastradoss
+           </Typography>
+          </Link>  
+          </Link>
+        </Box>
+      </Drawer>
+    </Box>
   );
-};
-
-export default Header;
+}
